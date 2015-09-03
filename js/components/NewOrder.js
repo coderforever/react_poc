89145f969b4b4362e004729fc5307b113b6cfb47
frank.shaof <fshao@splunk.com>
2015-09-03 12:00:02 +0800

    button style

--- js/components/NewOrder.js
+++ js/components/NewOrder.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import OrderActions from '../actions/OrderActions';
 import OrderConstants from '../constants/OrderConstants';
+import { Button } from 'react-bootstrap';
 
 export default class NewOrder extends React.Component {
     constructor(){
@@ -18,15 +19,11 @@ export default class NewOrder extends React.Component {
         return (
             <div className='create_div'>
                 <form onSubmit={this._submitOrder.bind(this)}>
-                    <div className='create_row'>
-                        <label htmlFor='orderName_input' className='submit_label'>订单名称: </label>
-                        <input type='text' placeholder={'请输入订单名称（不超过'+OrderConstants.ORDER_NAME_LIMIT+'字）'} value={this.state.orderName} id='orderName_input' className={this.state.orderName_validate ? '' : 'error'} onChange={this._nameInputChange.bind(this)}/>
-                    </div>
-                    <div className='create_row'>
-                        <label htmlFor='orderDesc_input' className='submit_label'>描述信息: </label>
-                        <textarea placeholder={'请输入描述信息（不超过'+OrderConstants.ORDER_DESC_LIMIT+'字）'} value={this.state.orderDescription} id='orderDesc_input' className={this.state.orderDesc_validate ? '' : 'error'} onChange={this._descInputChange.bind(this)}/>
-                    </div>
-                    <input type='submit' value='确认创建' id='create_submit'/>
+                    <label htmlFor='orderName_input' className='submit_label'>订单名称: </label><br/>
+                    <input type='text' placeholder={'请输入订单名称（不超过'+OrderConstants.ORDER_NAME_LIMIT+'字）'} value={this.state.orderName} id='orderName_input' className={this.state.orderName_validate ? '' : 'error'} onChange={this._nameInputChange.bind(this)}/><br/>
+                    <label htmlFor='orderDesc_input' className='submit_label'>描述信息: </label><br/>
+                    <textarea placeholder={'请输入描述信息（不超过'+OrderConstants.ORDER_DESC_LIMIT+'字）'} value={this.state.orderDescription} id='orderDesc_input' className={this.state.orderDesc_validate ? '' : 'error'} onChange={this._descInputChange.bind(this)}/><br/>
+                    <Button bsStyle='success' type='submit' id='create_submit'>确认创建</Button>
                 </form>
             </div>
         );
