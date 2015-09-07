862e05e43f96675dc03f4634f7fdc2a926dff01b
frank.shaof <fshao@splunk.com>
2015-09-08 00:09:02 +0800

    bug fix

--- js/NewOrderApp.js
+++ js/NewOrderApp.js
@@ -2,10 +2,11 @@ import React from 'react';
 import NewOrder from './components/NewOrder';
 import OrderMenu from './components/OrderMenu';
 import LogoFooter from './components/LogoFooter';
+import UserConstants from './constants/UserConstants';
 import PermissionCheck from './PermissionCheck';
 
-PermissionCheck.loginCheck();
+PermissionCheck.loginCheck(UserConstants.CUSTOMER_LOGIN_URL);
 
-React.render(<OrderMenu />, document.getElementById('menu_bar'));
+React.render(<OrderMenu role={UserConstants.CUSTOMER_ROLE} />, document.getElementById('menu_bar'));
 React.render(<NewOrder />, document.getElementById('new_order_panel'));
 React.render(<LogoFooter />, document.getElementById('logo-footer'));
--- js/components/OrderMenu.js
+++ js/components/OrderMenu.js
@@ -8,7 +8,7 @@ export default class OrderMenu extends React.Component {
             <div className='orderMenuBar'>
                 <div className='menuGroup'>
                     <a className='menuItem' href={ this.props.role==UserConstants.CUSTOMER_ROLE ? '/customerOrderList.html' : '/venderOrderList.html' }>订单管理</a>
-                    { this.props.role==UserConstants.VENDER_ROLE ? '': (<a className='menuItem' href='/newOrder.html'>创建订单</a>) }
+                    { this.props.role==UserConstants.CUSTOMER_ROLE ? (<a className='menuItem' href='/newOrder.html'>创建订单</a>) : '' }
                     <a className='menuItem personInfo'><span className="glyphicon glyphicon-user"></span></a>
                 </div>
             </div>
