4a31c58aff7680bb27c5c2bf1ad724f492ec28f0
frank.shaof <fshao@splunk.com>
2015-09-10 21:27:59 +0800

    foot deleted.

--- js/NewOrderApp.js
+++ js/NewOrderApp.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import NewOrder from './components/NewOrder';
 import OrderMenu from './components/OrderMenu';
-import LogoFooter from './components/LogoFooter';
 import UserConstants from './constants/UserConstants';
 import PermissionCheck from './PermissionCheck';
 
@@ -9,4 +8,3 @@ PermissionCheck.loginCheck(UserConstants.CUSTOMER_LOGIN_URL);
 
 React.render(<OrderMenu role={UserConstants.CUSTOMER_ROLE} />, document.getElementById('menu_bar'));
 React.render(<NewOrder />, document.getElementById('new_order_panel'));
-React.render(<LogoFooter />, document.getElementById('logo-footer'));
