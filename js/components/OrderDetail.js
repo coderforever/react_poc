3e95c5e0199178a64bfacf4dc37575a169e185b2
darlingtld <darlingtld@qq.com>
2015-09-17 16:28:05 +0800

    ui change

--- js/components/OrderDetail.js
+++ js/components/OrderDetail.js
@@ -5,21 +5,24 @@ import OrderStore from '../stores/OrderStore';
 export default class OrderDetail extends React.Component {
 
     render() {
-        let search=location.search, regex = /^\?id=(\d+)$/, results = regex.exec(search);
+        let search = location.search, regex = /^\?id=(\d+)$/, results = regex.exec(search);
 
-        if (results != null && results.length==2) {
-            let order=OrderStore.getOrder(results[1]);
+        if (results != null && results.length == 2) {
+            let order = OrderStore.getOrder(results[1]);
 
             return (
                 <div className='orderDetail'>
                     <div className='orderTitle'>{order.name}</div>
                     <div className='orderDesc'>{order.description}</div>
-                    <a className='red_btn' onClick={()=>this._deleteOrder(id)} href='javascript:;'>删除</a>
-                    <a className='red_btn' onClick={()=>this._rejectOrder(id)} href='javascript:;'>拒绝订单</a>
-                    <a className='normal_btn' onClick={()=>this._confirmOrder(id)} href='javascript:;'>确认完成</a>
-                    <a className='normal_btn' onClick={()=>this._completeOrder(id)} href='javascript:;'>完成订单</a>
-                    <a className='normal_btn' onClick={()=>this._acceptOrder(id)} href='javascript:;'>接受订单</a>
-                    <div style={{clear:'both'}}></div>
+                    <div className='orderAcceptOps'>
+                        <a className='normal_btn' onClick={()=>this._confirmOrder(id)} href='javascript:;'>确认完成</a>
+                        <a className='normal_btn' onClick={()=>this._completeOrder(id)} href='javascript:;'>完成订单</a>
+                        <a className='normal_btn' onClick={()=>this._acceptOrder(id)} href='javascript:;'>接受订单</a>
+                    </div>
+                    <div className='orderDenialOps'>
+                        <a className='red_btn' onClick={()=>this._deleteOrder(id)} href='javascript:;'>删除</a>
+                        <a className='red_btn' onClick={()=>this._rejectOrder(id)} href='javascript:;'>拒绝订单</a>
+                    </div>
                 </div>
             );
         }
