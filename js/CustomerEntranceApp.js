74c32857bfde1e3578ded1b964e9f3d880d015a9
darlingtld <darlingtld@qq.com>
2015-09-18 17:55:43 +0800

    entrance module

--- js/CustomerEntranceApp.js
+++ js/CustomerEntranceApp.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import UserConstants from './constants/UserConstants';
 import Entrance from './components/Entrance';
+import UserConstants from './constants/UserConstants';
+import PermissionCheck from './PermissionCheck';
+
+PermissionCheck.loginCheck(UserConstants.CUSTOMER_LOGIN_URL);
 
 React.render(<Entrance/>, document.getElementById('entrance-wrapper'));
 
--- js/components/Entrance.js
+++ js/components/Entrance.js
@@ -4,28 +4,36 @@ import { Col, Grid, Row, Glyphicon } from 'react-bootstrap';
 export default class Entrance extends React.Component {
 
     render() {
-        const phoneIcon = <Glyphicon glyph='phone'/>;
-        const passwordIcon = <Glyphicon glyph='lock'/>;
         return (
             <Grid>
                 <Row className="show-grid">
                     <Col xs={8}>
-                        <div className='my-profile'><span className='module-name'>我的信息</span></div>
+                        <a herf='#'>
+                            <div className='my-profile'><span className='module-name'>我的信息</span></div>
+                        </a>
                     </Col>
                     <Col xs={4}>
-                        <div className='my-order'><span className='module-name'>我的订单</span></div>
+                        <a href='/customerOrderList.html'>
+                            <div className='my-order'><span className='module-name'>我的订单</span></div>
+                        </a>
                     </Col>
                 </Row>
 
                 <Row className="show-grid">
                     <Col xs={4}>
-                        <div className='service'><span className='module-name'>汽车服务</span></div>
+                        <a herf='#'>
+                            <div className='service'><span className='module-name'>汽车服务</span></div>
+                        </a>
                     </Col>
                     <Col xs={4}>
-                        <div className='service'><span className='module-name'>手机服务</span></div>
+                        <a herf='#'>
+                            <div className='service'><span className='module-name'>手机服务</span></div>
+                        </a>
                     </Col>
                     <Col xs={4}>
-                        <div className='service'><span className='module-name'>其他服务</span></div>
+                        <a herf='#'>
+                            <div className='service'><span className='module-name'>其他服务</span></div>
+                        </a>
                     </Col>
                 </Row>
 
